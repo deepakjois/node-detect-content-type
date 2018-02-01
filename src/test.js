e9e376985f9b0c9c6a1892531844654cf1bd4b07
Deepak Jois <deepak.jois@gmail.com>
2018-02-01 20:51:50 +0530

    Porting all test cases over

--- src/test.js
+++ src/test.js
@@ -3,7 +3,121 @@ import { describe, it } from 'mocha'
 import assert from 'assert'
 
 const sniffTests = [
+  // Some nonsense.
+  {
+    desc: 'Empty',
+    data: Buffer.from([]),
+    contentType: 'text/plain; charset=utf-8'
+  },
+  {
+    desc: 'Binary',
+    data: Buffer.from([1, 2, 3]),
+    contentType: 'application/octet-stream'
+  },
+
+  {
+    desc: 'HTML document #1',
+    data: Buffer.from(`<HtMl><bOdY>blah blah blah</body></html>`),
+    contentType: 'text/html; charset=utf-8'
+  },
+  {
+    desc: 'HTML document #2',
+    data: Buffer.from(`<HTML></HTML>`),
+    contentType: 'text/html; charset=utf-8'
+  },
+  {
+    desc: 'HTML document #3 (leading whitespace)',
+    data: Buffer.from('   <!DOCTYPE HTML>...'),
+    contentType: 'text/html; charset=utf-8'
+  },
+  {
+    desc: 'HTML document #4 (leading CRLF)',
+    data: Buffer.from('\r\n<html>...'),
+    contentType: 'text/html; charset=utf-8'
+  },
+  {
+    desc: 'Plain text',
+    data: Buffer.from('This is not HTML. It has ☃ though.', 'utf8'),
+    contentType: 'text/plain; charset=utf-8'
+  },
+
   { desc: 'PDF', data: Buffer.from('%PDF-'), contentType: 'application/pdf' },
+
+  {
+    desc: 'XML',
+    data: Buffer.from('\n<?xml!'),
+    contentType: 'text/xml; charset=utf-8'
+  },
+
+  // Image types.
+  { desc: 'GIF 87a', data: Buffer.from(`GIF87a`), contentType: 'image/gif' },
+  { desc: 'GIF 89a', data: Buffer.from(`GIF89a...`), contentType: 'image/gif' },
+
+  // Audio types.
+  {
+    desc: 'MIDI audio',
+    data: Buffer.concat([
+      Buffer.from('MThd'),
+      Buffer.from([0x00, 0x00, 0x00, 0x06, 0x00, 0x01])
+    ]),
+    contentType: 'audio/midi'
+  },
+  {
+    desc: 'MP3 audio/MPEG audio',
+    data: Buffer.concat([
+      Buffer.from('ID3'),
+      Buffer.from([0x03, 0x00, 0x00, 0x00, 0x00, 0x0f])
+    ]),
+    contentType: 'audio/mpeg'
+  },
+  {
+    desc: 'WAV audio #1',
+    data: Buffer.concat([
+      Buffer.from('RIFFb'),
+      Buffer.from([0xb8, 0x00, 0x00]),
+      Buffer.from('WAVEfmt '),
+      Buffer.from([0x12, 0x00, 0x00, 0x00, 0x06])
+    ]),
+    contentType: 'audio/wave'
+  },
+  {
+    desc: 'WAV audio #2',
+    data: Buffer.concat([
+      Buffer.from('RIFF'),
+      Buffer.from([0x00, 0x00, 0x00]),
+      Buffer.from('WAVEfmt '),
+      Buffer.from([0x12, 0x00, 0x00, 0x00, 0x06])
+    ]),
+    contentType: 'audio/wave'
+  },
+  {
+    desc: 'AIFF audio #1',
+    data: Buffer.concat([
+      Buffer.from('FORM'),
+      Buffer.from([0x00, 0x00, 0x00, 0x00]),
+      Buffer.from('AIFFCOMM'),
+      Buffer.from([
+        0x00,
+        0x00,
+        0x00,
+        0x12,
+        0x00,
+        0x01,
+        0x00,
+        0x00,
+        0x57,
+        0x55,
+        0x00,
+        0x10,
+        0x40,
+        0x0d,
+        0xf3,
+        0x34
+      ])
+    ]),
+    contentType: 'audio/aiff'
+  },
+
   {
     desc: 'Ogg audio',
     data: Buffer.concat([
@@ -42,10 +156,22 @@ const sniffTests = [
     contentType: 'application/ogg'
   },
   {
-    desc: 'HTML document #3 (leading whitespace)',
-    data: Buffer.from('   <!DOCTYPE HTML>...'),
-    contentType: 'text/html; charset=utf-8'
+    desc: 'Must not match OGG',
+    data: Buffer.from('owow\x00'),
+    contentType: 'application/octet-stream'
   },
+  {
+    desc: 'Must not match OGG',
+    data: Buffer.from('oooS\x00'),
+    contentType: 'application/octet-stream'
+  },
+  {
+    desc: 'Must not match OGG',
+    data: Buffer.from('oggS\x00'),
+    contentType: 'application/octet-stream'
+  },
+
+  // Video types.
   {
     desc: 'MP4 video',
     data: Buffer.concat([
@@ -59,9 +185,14 @@ const sniffTests = [
     contentType: 'video/mp4'
   },
   {
-    desc: 'Plain text',
-    data: Buffer.from('This is not HTML. It has ☃ though.', 'utf8'),
-    contentType: 'text/plain; charset=utf-8'
+    desc: 'AVI video #1',
+    data: Buffer.from('RIFF,O\n\x00AVI LISTÀ'),
+    contentType: 'video/avi'
+  },
+  {
+    desc: 'AVI video #2',
+    data: Buffer.from('RIFF,\n\x00\x00AVI LISTÀ'),
+    contentType: 'video/avi'
   }
 ]
 
